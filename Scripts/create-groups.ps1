9d4618c9af8340a7db26c10667b973d9b429dbd7
matszook <mats.h.haartveit@gmail.com>
2026-06-09 13:00:24 +0200

    Update create-groups.ps1

--- Scripts/create-groups.ps1
+++ Scripts/create-groups.ps1
@@ -2,16 +2,29 @@ Import-Module ActiveDirectory
 
 $users = Import-Csv "..\Data\nye-elever.csv"
 
+# Opprett gruppe (kan også sjekkes først)
+if (-not (Get-ADGroup -Filter "Name -eq 'Elever-Access'" -ErrorAction SilentlyContinue)) {
+    New-ADGroup -Name "Elever-Access" -GroupScope Global -GroupCategory Security -Path "OU=IT,DC=eksamen,DC=lab"
+}
+
 foreach ($user in $users) {
+
     $username = ($user.Fornavn.Substring(0,1) + $user.Etternavn).ToLower()
-    
-    Add-ADGroupMember -Identity "Elever-Access" -Members $username
 
-    if(Get-ADUSer -Filter "SamAccountName -eq '$username'" -ErrorAction SilentlyContinue) {
-        ADGroupMember -Identity "Elever-Access" -Members $username
+    # SJEKK: finnes brukeren i AD
+    $adUser = Get-ADUser -Filter "SamAccountName -eq '$username'" -ErrorAction SilentlyContinue
+
+    if ($adUser) {
+
+        # SJEKK: er allerede medlem
+        $isMember = Get-ADGroupMember "Elever-Access" | Where-Object { $_.SamAccountName -eq $username }
+
+        if (-not $isMember) {
+            Add-ADGroupMember -Identity "Elever-Access" -Members $username
+            Write-Host "Lagt til i gruppe: $username"
+        }
     }
-
-    if (!(Get-ADGroupMember "Elever-Access" | Where-Object {$_.SamAccountName -eq $username})) {
-        Add-ADGroupMember -Identity "Elever-Access" -Members $username
+    else {
+        Write-Host "Bruker finnes ikke i AD: $username"
     }
 }
